53e9bd70ffb22c089e3371d6e553b4be9aca8630
yeahLim <yeahleem2@gmail.com>
2023-05-18 14:05:21 +0900

    프로젝트 생성

---
+++ mytrip/src/main/java/com/kiki/MytripApplication.java
@@ -0,0 +1,31 @@
+package com.kiki;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@Configuration
+@MapperScan(basePackages = {"com.kiki.**.dao"})
+public class MytripApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytripApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+
+	
+}
