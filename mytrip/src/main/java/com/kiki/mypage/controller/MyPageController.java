9d17ca4acc4aff4b968ed43483fab92035f9d0ea
yeahLim <yeahleem2@gmail.com>
2023-05-22 00:18:50 +0900

    [#6] mypage controller 수정
    230522 yeahLim

--- mytrip/src/main/java/com/kiki/mypage/controller/MyPageController.java
+++ mytrip/src/main/java/com/kiki/mypage/controller/MyPageController.java
@@ -1,20 +1,47 @@
 package com.kiki.mypage.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.kiki.user.model.service.UserService;
+import com.kiki.mypage.model.service.MyPageService;
 
 @Controller
 @RequestMapping("mypage")
 public class MyPageController {
 	@Autowired
-	UserService userService;
+	MyPageService myPageService;
 	
 	@GetMapping("/mypage")
 	public String mypage() {
-		return "mypage/mypage"; // id로 이동할 수 있도록 처리  // 계정과 일치하는 사람만 이동하도록 처리
+		return "mypage/mypage";
+	}
+	
+	@GetMapping("/authentication")
+	public String authentication() {
+		return "mypage/authentication";
+	}
+	
+	@PostMapping("/authentication")
+	public String authenticateUser(@RequestParam(value="password") String password, HttpSession session, Model model) {
+		try {
+			int validation = myPageService.authenticateUser(password);
+			if (validation == 0) {
+				model.addAttribute("mypageMsg", "비밀번호가 틀렸습니다.");
+				return "mypage/authentication";
+			} else {
+				return "mypage/update";
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			return "error/error";
+		}
 	}
 }
+
