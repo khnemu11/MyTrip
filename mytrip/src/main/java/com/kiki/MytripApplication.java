4bbad432c2506a2c6643a570d62a3ab922acb33a
khnemu <khnemu@naver.com>
2023-06-09 17:57:41 +0900

    [#10] feat : war 배포 코드 추가

--- mytrip/src/main/java/com/kiki/MytripApplication.java
+++ mytrip/src/main/java/com/kiki/MytripApplication.java
@@ -3,6 +3,8 @@ package com.kiki;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -11,12 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @Configuration
 @MapperScan(basePackages = {"com.kiki.**.dao"})
-public class MytripApplication implements WebMvcConfigurer {
+public class MytripApplication extends SpringBootServletInitializer implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MytripApplication.class, args);
 	}
-
+	//war 배포를 위한 메소드
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MytripApplication.class);
+	}
 	// 정적 파일 경로 추가
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
